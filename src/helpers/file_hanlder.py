af9d9aee20d200d12ce93b781aab4369826df231
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-12-05 14:59:08 +0100

    File handler for csv and normal files

---
+++ src/helpers/file_hanlder.py
@@ -0,0 +1,28 @@
+from init.packages import os
+from init.packages import csv
+
+def check_if_file_exists(file_path: str) -> bool:
+	if not os.path.exists(file_path):
+		return False
+	return True
+
+def read_file_content(file_path: str) -> str:
+	if not check_if_file_exists(file_path):
+		raise Exception(f"File: \"{file_path}\" does not exist!\nCheck the file and try again.")
+	with open(file_path, "r") as f:
+		content = f.read()
+		f.close()
+		return content
+
+def read_csv_file(file_path: str, line: int) -> list:
+	if not check_if_file_exists(file_path):
+		raise Exception(f"File: \"{file_path}\" does not exist!\nCheck the file and try again.")
+	with open(file_path) as csv_obj:
+		csv_reader = csv.reader(csv_obj, delimiter=',')
+		header = next(csv_reader, None)
+		row = []
+		if header is not None:
+			csv_reader = list(csv_reader)
+			row = csv_reader[line]
+			return row
+		return row
