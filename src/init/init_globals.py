b7c8d7626b0e04d6a1e9d8b1bebd98614e26a8da
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-12-05 14:59:28 +0100

    Add ERRORS for globals

--- src/init/init_globals.py
+++ src/init/init_globals.py
@@ -1,4 +1,4 @@
-import os
+from init.packages import os
 
 class PATHS:
 	SEP = os.path.sep
@@ -7,3 +7,13 @@ class PATHS:
 	STORAGE = 'storage'
 	LOG_FILE = CWD + SEP + STORAGE + SEP + 'logs'
 	CHROME_DRIVER = CWD + SEP + ASSETS + SEP + 'chromedriver.exe'
+	RESOURCES_FILE = CWD + SEP + ASSETS + SEP + 'resources.csv'
+
+class ERRORS:
+	UNKNOWN_ERROR = -1
+	EMAIL_ERROR = 1
+	PASS_ERROR = 2
+	PROXY_ERROR = 3
+	CAPTCHA_ERROR = 4
+	BLOCKED_ACC_ERROR = 5
+	VERIFICATION_ERROR = 6
