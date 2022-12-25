1624d0e3aed075c59ad3b57a8bfc6b6a1e321921
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-12-25 16:01:26 +0100

    Add bounce_handler function

--- src/app/account_filter_handler.py
+++ src/app/account_filter_handler.py
@@ -30,6 +30,48 @@ def filter_options(browser: Chrome, lable_name: str, j:int):
 			continue
 	return ['', i]
 
+def bounce_handler(browser: Chrome, timeout: int):
+	filter_xpath = '//*[@id="aso_search_form_anchor"]/button[2]'
+	try:
+		# Filter icon in search
+		filter_actions(browser, timeout, filter_xpath, False, '')
+		# From name input
+		i = 0
+		browser_handler.wait_time_in_range(3.1, 5.3)
+		while (i:=i+1):
+			xpath = f'/html/body/div[{i}]/div/div[2]/div[1]/span[1]/label'
+			try:
+				browser_handler.wait_time_in_range(.1, .3)
+				element = browser.find_element(By.XPATH, xpath)
+				if element.text == 'From':
+					xpath = f'/html/body/div[{i}]/div/div[2]/div[1]/span[2]/input'
+					filter_actions(browser, timeout, xpath, True, 'Mail Delivery Subsystem')
+					break
+				if i > 30:
+					break
+			except:
+				if i > 30:
+					break
+				continue
+		# Create filter button
+		xpath = f'/html/body/div[{i}]/div/div[2]/div[10]/div[2]'
+		browser_handler.wait_time_in_range(1.1, 3.3)
+		filter_actions(browser, timeout, xpath, False, '')
+		xpath = filter_options(browser, 'Categorize as:', 10)
+		if xpath[0] != '':
+			xpath[0] = xpath[0].replace('label', 'div/div[1]')
+			filter_actions(browser, timeout, xpath[0], False, '')
+			browser_handler.wait_time_in_range(1.1, 2.2)
+			xpath[0] = xpath[0].replace('div/div[1]', 'div[2]/div[3]')
+			filter_actions(browser, timeout, xpath[0], False, '')
+		xpath[0] = f'/html/body/div[{xpath[1]}]/div/div[2]/div[4]/div'
+		browser_handler.wait_for_element_by_xpath(browser, timeout, xpath[0])
+		element = browser.find_element(By.XPATH, xpath[0])
+		element.click()
+		browser_handler.wait_time_in_range(2.0, 3.0)
+	except:
+		pass
+
 def filter_handler(browser: Chrome, timeout: int, accepted_from: str):
 	browser.get('https://mail.google.com/mail/u/0/#inbox')
 	filter_xpath = '//*[@id="aso_search_form_anchor"]/button[2]'
@@ -55,6 +97,7 @@ def filter_handler(browser: Chrome, timeout: int, accepted_from: str):
 					break
 				continue
 		# Create filter button
+		browser.find_element(By.XPATH, f'/html/body/div[{i}]/div/div[2]/div[1]/span[1]/label').click()
 		xpath = f'/html/body/div[{i}]/div/div[2]/div[10]/div[2]'
 		browser_handler.wait_time_in_range(1.1, 3.3)
 		filter_actions(browser, timeout, xpath, False, '')
@@ -77,6 +120,7 @@ def filter_handler(browser: Chrome, timeout: int, accepted_from: str):
 		element = browser.find_element(By.XPATH, xpath[0])
 		element.click()
 		browser_handler.wait_time_in_range(2.0, 3.0)
+		bounce_handler(browser, timeout)
 	except Exception as e:
 		print(e)
 	
