a7e2ad9b14bf1d0dd0bc082e7e8c1c43cde96ee0
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-12-29 14:24:08 +0100

    Edit account module

--- src/modules/Account.py
+++ src/modules/Account.py
@@ -4,27 +4,12 @@ class Account:
 	def __init__(self):
 		self.email = ""
 		self.password = ""
-		self.recovery = ""
-		self.proxyIp = "0.0.0.0"
-		self.proxyPort = -1
-		self.proxyUser = ""
-		self.proxyPassword = ""
 
 	# Getters
 	def getEmail(self):
 		return self.email
 	def getPassword(self):
 		return self.password
-	def getRecovery(self):
-		return self.recovery
-	def getProxyIp(self):
-		return self.proxyIp
-	def getProxyPort(self):
-		return self.proxyPort
-	def getProxyUser(self):
-		return self.proxyUser
-	def getProxyPassword(self):
-		return self.proxyPassword
 
 	# Setters
 	def setEmail(self, email):
@@ -37,25 +22,3 @@ class Account:
 			raise Exception("Email password is empty!")
 		else:
 			self.password = password
-	def setRecovery(self, recovery):
-		self.recovery = recovery
-	def setProxyIp(self, proxyIp):
-		if re.fullmatch('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', proxyIp) == None:
-			raise Exception("Proxy must be valid!")
-		else:
-			self.proxyIp = proxyIp
-	def setProxyPort(self, port: int):
-		if port in range(0, 65535):
-			self.proxyPort = port
-		else:
-			raise Exception("Proxy's port must be between 0 and 65535!")
-	def setProxyUser(self, username):
-		if username == "" or username ==None:
-			raise Exception("Proxy user must be specified!")
-		else:
-			self.proxyUser = username
-	def setProxyPassword(self, password):
-		if password == "" or password == None:
-			raise Exception("Proxy password must be specified!")
-		else:
-			self.proxyPassword = password
