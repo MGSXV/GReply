1510a88dca1d2c283ed5b708e8c5947faea99196
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-01-03 16:38:49 +0100

    init webdriver updated

--- src/init/init_webdriver.py
+++ src/init/init_webdriver.py
@@ -3,20 +3,21 @@ from selenium.webdriver import ChromeOptions
 from fake_useragent import UserAgent
 from init.init_globals import PATHS, COLORS
 from modules.Account import Account
+from modules.Profile import Profile
 from helpers import file_hanlder
 
-def generate_proxy_extension(account: Account):
-	sub_dir = PATHS.STORAGE + PATHS.SEP + account.getEmail().split('@')[0]
+def generate_proxy_extension(profile: Profile):
+	sub_dir = PATHS.STORAGE + PATHS.SEP + profile.getName()
 	file_hanlder.create_dir_if_not_exist(sub_dir)
-	sub_dir = PATHS.STORAGE + PATHS.SEP + account.getEmail().split('@')[0] + PATHS.SEP + 'proxy_extension'
+	sub_dir = PATHS.STORAGE + PATHS.SEP + profile.getName() + PATHS.SEP + 'proxy_extension'
 	file_hanlder.create_dir_if_not_exist(sub_dir)
 	try:
 		manifest_content = file_hanlder.read_file_content(PATHS.MANIFEST_TEMPLATE_FILE)
 		script_content = file_hanlder.read_file_content(PATHS.SCRIPT_TEMPLATE_FILE) % {
-			"host": account.getProxyIp(),
-            "port": account.getProxyPort(),
-            "user": account.getProxyUser(),
-            "pass": account.getProxyPassword(),
+			"host": profile.proxy.getIp(),
+            "port": profile.proxy.getPort(),
+            "user": profile.proxy.getUser(),
+            "pass": profile.proxy.getPassword(),
 		}
 		if not file_hanlder.write_file(sub_dir + PATHS.SEP + 'manifest.json', manifest_content, 'w'):
 			return None
@@ -27,26 +28,28 @@ def generate_proxy_extension(account: Account):
 		print(e)
 		exit(1)
 
-def init_webdriver(account: Account, config) -> Chrome:
+def init_webdriver(profile: Profile, config) -> Chrome:
 	ua = UserAgent()
-	sub_dir = account.getEmail().split('@')[0]
+	sub_dir = profile.getName()
 	userAgent = ua.random
 	chrome_options = ChromeOptions()
 	chrome_options.add_argument("--lang=en-US")
 	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-	# chrome_options.add_argument(f'--user-data-dir={PATHS.CHROME_SYS_PATH}')
-	# chrome_options.add_argument(f'--profile-directory={sub_dir}')
+	chrome_options.add_argument(f'--user-data-dir={PATHS.CHROME_SYS_PATH}')
+	chrome_options.add_argument(f'--profile-directory={sub_dir}')
 	chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
 	chrome_options.add_experimental_option('excludeSwitches', ['enable-logging', 'enable-automation'])
 	chrome_options.add_experimental_option("useAutomationExtension", False)
-	# chrome_options.add_experimental_option("excludeSwitche`s",[""])
-	# if config['proxy'] == 'true':
-	# 	if account.getProxyIp() != '0.0.0.0' or account.getProxyPort() != -1:
-	# 		extention = generate_proxy_extension(account)
-	# 		if extention is None:
-	# 			print('Error while initializing webdriver!')
-	# 			return None
-	# 	chrome_options.add_argument(f'--load-extension={PATHS.CWD + PATHS.SEP + extention}')
-	chrome_dirver = Chrome(options=chrome_options, executable_path=PATHS.CHROME_DRIVER)
-	# COLORS.success_messgae('Initializing webdriver is done!')
-	return chrome_dirver
+	if config['proxy'] == 'true':
+		if profile.proxy.getIp() != '0.0.0.0' or profile.proxy.getPort() != -1:
+			extention = generate_proxy_extension(profile)
+			if extention is None:
+				print('Error while initializing webdriver!')
+				return None
+		chrome_options.add_argument(f'--load-extension={PATHS.CWD + PATHS.SEP + extention}')
+	try:
+		chrome_dirver = Chrome(options=chrome_options, executable_path=PATHS.CHROME_DRIVER)
+		return chrome_dirver
+	except:
+		print(f'{COLORS.RED}Unkown error!{COLORS.DEFAULT}')
+		exit(1)
