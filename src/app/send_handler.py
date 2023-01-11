83a7728b91e2efa91ce3cff3b06d2c546810453f
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-01-11 16:08:06 +0100

    Remove unwanted functions!

--- src/app/send_handler.py
+++ src/app/send_handler.py
@@ -2,26 +2,12 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from modules.Account import Account
 from selenium.webdriver.common.action_chains import ActionChains
 from datetime import datetime
 from helpers import browser_handler
 from init.init_globals import PATHS, ERRORS
 from helpers.logs_handler import Logger
-
-def get_number_of_emails(browser: Chrome, timeout: int):
-	xpath = '/html/body/div[7]/div[3]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[1]/div[2]/div[1]/span/div[1]/span'
-	acc_info = {'total': 450, 'epp': 50, 'page': 1}
-	try:
-		browser_handler.wait_for_element_by_xpath(browser, timeout, xpath)
-		element = browser.find_element(By.XPATH, xpath)
-		num = element.text
-		_num = num.split('of')
-		acc_info['total'] = int(_num[1].strip().replace(',', ''))
-		acc_info['epp'] = int(_num[0].split('–')[1].strip().replace(',', ''))
-		acc_info['page'] = int(_num[0].split('–')[0].strip().replace(',', ''))
-	except Exception as e:
-		print(e)
-	return acc_info
 
 def locate_email(browser: Chrome, timeout: int, accepted_from: str, xpath: str) -> WebElement or None:
 	try:
@@ -35,55 +21,34 @@ def locate_email(browser: Chrome, timeout: int, accepted_from: str, xpath: str) 
 	except Exception as e:
 		return None
 
-def get_creative(browser: Chrome, timeout: int):
+def get_creative(browser: Chrome, timeout: int, index: int):
 	action = ActionChains(browser)
 	xpath ='/html/body/div[7]/div[3]/div/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/table/tr/td/div[2]/div[2]/div/div[3]/div/div/div/div/div/div[2]/div/div/div/div[4]/table/tbody/tr/td[2]/table/tbody/tr[1]/td/div/div[2]/div[3]/div/table/tbody/tr/td[2]/div[2]/div/div[1]'
 	browser_handler.wait_for_element_by_xpath(browser, timeout, xpath)
 	element = browser.find_element(By.XPATH, xpath)
-	browser.execute_script('window.open("")')
-	browser.switch_to.window(browser.window_handles[1])
-	browser.get(PATHS.CREATIVE)
+	browser.switch_to.window(browser.window_handles[0])
+	browser.refresh()
 	browser_handler.wait_time_in_range(0.7, 1.0)
 	action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).perform()
 	browser_handler.wait_time_in_range(0.2, 0.5)
 	action.key_down(Keys.CONTROL).send_keys('c').key_up(Keys.CONTROL).perform()
-	browser.execute_script('window.close()')
-	browser.switch_to.window(browser.window_handles[0])
+	browser.switch_to.window(browser.window_handles[index])
 	element.click()
 	browser_handler.wait_time_in_range(0.5, 0.9)
 	action.key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
 	browser_handler.wait_time_in_range(0.5, 0.9)
 	ActionChains(browser).key_down(Keys.CONTROL).send_keys(Keys.ENTER).key_up(Keys.CONTROL).perform()
 
-def send_reply(browser: Chrome, timeout: int):
+def send_reply(browser: Chrome, timeout: int, index: int):
 	xpath = '/html/body/div[7]/div[3]/div/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/table/tr/td/div[2]/div[2]/div/div[3]/div/div/div/div/div/div[1]/div[2]/div[1]/table/tbody/tr[1]/td[4]/div[1]'
 	try:
 		browser_handler.wait_for_element_by_xpath(browser, timeout, xpath)
 		element = browser.find_element(By.XPATH, xpath)
 		element.click()
 		browser_handler.wait_time_in_range(1.1, 1.5)
-		get_creative(browser, timeout)
+		get_creative(browser, timeout, index)
 	except Exception as e:
 		print(e)
-
-def get_send_date(browser: Chrome, timeout: int, xpath: str):
-	try:
-		browser_handler.wait_for_element_by_xpath(browser, timeout, xpath)
-		element = browser.find_element(By.XPATH, xpath)
-		email_date = element.get_attribute('aria-label')
-		date_compounents = email_date.split(', ')
-		day_parse = date_compounents[1].split(' ')
-		if len(day_parse[1]) == 1:
-			day_parse[1] = '0' + day_parse[1]
-		date_compounents[1] = day_parse[0] + ' ' + day_parse[1]
-		_date = f'{date_compounents[0]} {date_compounents[1]} {date_compounents[2]}'
-		dt = datetime.strptime(_date, '%a %b %d %Y')
-		return dt.date()
-	except Exception as e:
-		print(e)
-
-def is_old_mail(date1: str, date2: datetime.date):
-	return datetime.strptime(date1, '%Y-%m-%d').date() <= date2
 
 def back_to_inbox(browser: Chrome, timeout: int):
 	try:
@@ -93,58 +58,54 @@ def back_to_inbox(browser: Chrome, timeout: int):
 	except:
 		browser.get('https://mail.google.com/mail/u/0/#inbox')
 
-def get_older_mails(browser: Chrome, timeout: int) -> int:
-	try:
-		browser_handler.wait_for_element_by_xpath(browser, timeout, '/html/body/div[7]/div[3]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[1]/div[2]/div[1]/span/div[3]')
-		element = browser.find_element(By.XPATH, '/html/body/div[7]/div[3]/div/div[2]/div[2]/div/div/div/div/div[1]/div/div[1]/div[2]/div[1]/span/div[3]')
-		if not element.get_property('disabled'):
-			element.click()
-			return 1
-		return 0
-	except:
-		return -1
+def open_all_accounts(accounts: list, browser: Chrome, accepted_from: str):
+	i = 1
+	for acc in accounts:
+		browser.execute_script('''window.open("about:blank");''')
+		browser.switch_to.window(browser.window_handles[i])
+		_from = accepted_from.replace(' ', '+')
+		old_url = f'https://mail.google.com/mail/u/{i - 1}/#search/is%3Aunread+from%3A({_from})'
+		browser.get(old_url)
+		browser_handler.wait_time_in_range(1.5, 3.5)
+		new_url = browser.current_url
+		if new_url != old_url:
+			browser.execute_script("window.close('','_parent','');")
+			browser.switch_to.window(browser.window_handles[0])
+			break
+		i = i + 1
+	return i - 1
 
-def wait_for_new_page(browser: Chrome, acc_info, timeout: int):
-	new_acc_info = get_number_of_emails(browser, timeout)
-	while new_acc_info == acc_info:
-		browser_handler.wait_time_in_range(1.0, 2.0)
-		new_acc_info = get_number_of_emails(browser, timeout)
+def get_active_accounts(accounts: list, browser: Chrome, timeout: int) -> list:
+	num_of_tabs = len(browser.window_handles)
+	accs = []
+	for i in range(num_of_tabs):
+		browser.switch_to.window(browser.window_handles[i])
+		for account in accounts:
+			if account.email.lower() in browser.title:
+				account.is_active = True
+				account.tab_index = i
+				accs.append(account)
+	return accs
 
-def send_proccess(browser: Chrome, timeout: int, accepted_from: str, email: str, config):
-	acc_info = get_number_of_emails(browser, timeout)
-	tbody_xpath = '/html/body/div[7]/div[3]/div/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div/div[8]/div/div[1]/div[2]/div/table/tbody'
-	try:
-		browser_handler.wait_for_element_by_xpath(browser, timeout, tbody_xpath)
-		i = 0
-		while (i:=i+1):
-			if i > acc_info['total']:
-				break
-			j = 0
-			if is_old_mail(config['send_date'], get_send_date(browser, timeout, f'{tbody_xpath}/tr[1]/td[8]/span')):
-				while (j:=j+1):
-					if j > acc_info['epp']:
-						break
-					element = locate_email(browser, timeout, accepted_from, f'{tbody_xpath}/tr[{j}]/td[4]')
-					if element is not None:
-						if is_old_mail(config['send_date'], get_send_date(browser, timeout, f'{tbody_xpath}/tr[{j}]/td[8]/span')):
-							browser_handler.wait_time_in_range(3.4, 5.5)
-							element.click()
-							browser_handler.wait_time_in_range(3.4, 5.5)
-							send_reply(browser, timeout)
-							browser_handler.wait_time_in_range(3.4, 5.5)
-							back_to_inbox(browser, timeout)
-						else:
-							return
+def send_group_send(accounts: list, browser: Chrome, timeout: int, accepted_from: str, config):
+	browser.get(PATHS.CREATIVE)
+	num_of_accs = open_all_accounts(accounts, browser, accepted_from)
+	accs_list = get_active_accounts(accounts, browser, timeout)
+	tbody_xpath = '/html/body/div[7]/div[3]/div/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div/div[5]/div[1]/div/table/tbody'
+	browser.switch_to.window(browser.window_handles[accs_list[0].tab_index])
+	while num_of_accs != 0:
+		for acc in accs_list:
+			browser.switch_to.window(browser.window_handles[acc.tab_index])
+			element = locate_email(browser, timeout, accepted_from, f'{tbody_xpath}/tr[{acc.email_index}]/td[4]')
+			if element is not None:
+				browser_handler.wait_time_in_range(3.4, 5.5)
+				element.click()
+				browser_handler.wait_time_in_range(3.4, 5.5)
+				send_reply(browser, timeout, acc.tab_index)
+				browser_handler.wait_time_in_range(3.4, 5.5)
+				back_to_inbox(browser, timeout)
 			else:
-				return
-			res = get_older_mails(browser, timeout)
-			if res == 0:
-				return
-			elif res == -1:
-				Logger.logger(ERRORS.PROXY_ERROR, email)
-				return
-			acc_info2 = get_number_of_emails(browser, timeout)
-			wait_for_new_page(browser, acc_info2, timeout)
-			i = i + acc_info['epp'] - 1
-	except Exception as e:
-		print(e)
+				browser.execute_script("window.close('','_parent','');")
+				accs_list = get_active_accounts(accounts, browser, timeout)
+				num_of_accs = num_of_accs - 1
+				break
