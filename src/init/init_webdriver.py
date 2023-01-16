18745fed4b2dae73a4ee4c786c4d1111dfb41ed3
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-01-16 17:24:49 +0100

    Login works now

--- src/init/init_webdriver.py
+++ src/init/init_webdriver.py
@@ -2,6 +2,7 @@ from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
 from fake_useragent import UserAgent
 from init.init_globals import PATHS, COLORS
+import undetected_chromedriver as uc
 from modules.Account import Account
 from modules.Profile import Profile
 from helpers import file_hanlder
@@ -34,13 +35,16 @@ def init_webdriver(profile: Profile, config) -> Chrome:
 	userAgent = ua.random
 	chrome_options = ChromeOptions()
 	chrome_options.add_argument("--lang=en-US")
+	chrome_options.add_argument("--disable-popup-blocking")
 	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+	# chrome_options.add_experimental_option('prefs', {'int`l.accept_languages': 'en,en_US'})
+	# chrome_options.add_experimental_option('excludeSwitches', ['enable-logging', 'enable-automation'])
+	# chrome_options.add_experimental_option("useAutomationExtension", False)
+	chrome_options.set_capability('dom.webdriver.enabled', False)
 	chrome_options.add_argument(f'--user-data-dir={PATHS.CHROME_SYS_PATH}')
 	chrome_options.add_argument(f'--profile-directory={sub_dir}')
-	chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
-	chrome_options.add_experimental_option('excludeSwitches', ['enable-logging', 'enable-automation'])
-	chrome_options.add_experimental_option("useAutomationExtension", False)
-	if config['proxy'] == 'true':
+	# chrome_options.binary_location = r'C:\Program Files\SRWare Iron (64-Bit)\chrome.exe'
+	if config['proxy']:
 		if profile.proxy.getIp() != '0.0.0.0' or profile.proxy.getPort() != -1:
 			extention = generate_proxy_extension(profile)
 			if extention is None:
@@ -48,8 +52,9 @@ def init_webdriver(profile: Profile, config) -> Chrome:
 				return None
 		chrome_options.add_argument(f'--load-extension={PATHS.CWD + PATHS.SEP + extention}')
 	try:
-		chrome_dirver = Chrome(options=chrome_options, executable_path=PATHS.CHROME_DRIVER)
+		chrome_dirver = uc.Chrome(options=chrome_options, executable_path=PATHS.CHROME_DRIVER)
 		return chrome_dirver
-	except:
+	except Exception as e:
+		print(e)
 		print(f'{COLORS.RED}Unkown error!{COLORS.DEFAULT}')
 		exit(1)
